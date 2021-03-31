0b83786c4fc07cf86bac167f359a4ca27d22004c
Roberto Sampaio <ssampaio.roberto@gmail.com>
2021-03-31 17:24:28 -0300

    Initial Commit

---
+++ Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by Roberto Sampaio on 31/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
