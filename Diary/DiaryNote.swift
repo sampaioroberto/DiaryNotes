a57d8e12231279081764bab4347d5831860bd7cd
Roberto Sampaio <ssampaio.roberto@gmail.com>
2021-04-12 09:01:42 -0300

    Change DiaryNote to Equatable

--- Diary/DiaryNote.swift
+++ Diary/DiaryNote.swift
@@ -1,13 +1,9 @@
 import Foundation
 
-struct DiaryNote: Hashable {
+struct DiaryNote: Equatable {
     let id = UUID()
     var title: String
     var message: String
-
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
 
     static func == (lhs: DiaryNote, rhs: DiaryNote) -> Bool {
         return lhs.id == rhs.id
